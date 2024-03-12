8b5b70238d8963da7c32f2269de19e58b091f494
Guido Piancastelli Ramos <guidotriturador@gmail.com>
2024-03-12 17:36:51 -0300

    libmatematica

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "matematica.h"
 
 int main(){
-    printf("Seno = %f\n", calcular_seno(M_PI_2));
+    printf("Seno = %f\n", calcular_seno(M_PI));
     printf("Cosseno = %f\n", calcular_cosseno(M_PI_2));
     printf("Tangente = %f\n", calcular_tangente(1));
 }
