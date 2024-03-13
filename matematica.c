73b0d99044f9b452a9b4b0cebd5296d466f27d13
guido-pr <guidotriturador@gmail.com>
2024-03-13 13:11:57 -0300

    libmatematica

--- matematica.c
+++ matematica.c
@@ -4,19 +4,19 @@
 
 
 double calcular_seno(double angulo_radianos){
-    return sin(angulo_radianos);
+    return sin(angulo_radianos); //Retorna o seno do angulo
 }
 
 double calcular_cosseno(double angulo_radianos){
-    return cos(angulo_radianos);
+    return cos(angulo_radianos); //Retorna o cosseno do angulo
 }
 
 double calcular_tangente(double angulo_radianos){
-    if (fabs(cos(angulo_radianos)) < 1e-10) {
-        printf("A tangente eh indefinida.\n");
-        return NAN;
+    if (fabs(cos(angulo_radianos)) < 1e-10){
+        printf("A tangente eh indefinida.\n"); //Verifica quando a tangente esta indefinida (Obs: é quando o cosseno = 0, já que tan = sen/cos)
+        return 0;
     }
     else{
-    return tan(angulo_radianos);
+    return tan(angulo_radianos); //Retorna a tangente do angulo
     }
 }
